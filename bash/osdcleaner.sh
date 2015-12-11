20c3d04a8c986e200bd71429d391d07182630474
tonycot <atarasov@mirantis.com>
2015-12-11 04:47:58 -0500

    Hot Fixes

--- bash/osdcleaner.sh
+++ bash/osdcleaner.sh
@@ -2,29 +2,29 @@
 #
 #
 echo "\n Welcome to OSD auto removing tool \n "
-read  -p "Please enter the OSD number like \"osd.1\" : " osd 
-echo "\n Your OSD is  $osd , starting... \n"
+read  -p "Please enter the OSD number like osd." nu
+echo "\n Your OSD is  osd.$nu , starting... \n"
 
 #Out OSD
-echo -e " \n $osd is going out \n"
+echo -e " \n osd.$nu is going out \n"
 sleep 5
 # stop OSD
-service ceph stop $osd
+service ceph stop osd.$nu
 sleep 5
 
 # Crush rm OSD
-echo -e " \n The $osd is going out from CRUSH \n "
-ceph osd crush remove $osd
+echo -e " \n The osd.$nu is going out from CRUSH \n "
+ceph osd crush remove osd.$nu
 sleep 5
 
 # Auth delete OSD
-echo -e " \n The Auth of $osd is going to be deleted \n "
-ceph auth del $osd
-sleep 5 
+echo -e " \n The Auth of osd.$nu is going to be deleted \n "
+ceph auth del osd.$nu
+sleep 5
 
 # Rm OSD
-echo -e " \n Deleting $osd \n"
-ceph osd rm $osd
+echo -e " \n Deleting osd.$nu \n"
+ceph osd rm osd.$nu
 sleep 5
 
 echo "Done"
