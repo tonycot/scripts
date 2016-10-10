484b8f78ed5b6f300a12f8b2811c423c018af69c
root <atarasov@mirantis.com>
2016-10-10 09:58:19 +0000

    Master SaltStack installation

--- bash/saltstack/masterinstall.sh
+++ bash/saltstack/masterinstall.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
 # Functions
-test
+
+function echos {
+  echo -e " \n  $1 $2"
+}
+
+function install {
+  for i in $1 $2 $3 $4
+    do
+      apt-get install -y $i
+    done
+}
+
+# Body 
+
+echos ...installing add-apt
+install python-software-properties software-properties-common
+add-apt-repository ppa:saltstack/salt
+apt-get update
+
+echos ...installing salt
+install salt-master salt-minion salt-ssh salt-cloud salt-doc
+echos ...done
